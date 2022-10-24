3d7d101ea46b0ea471581d075109e08f2bb4e968
shubhoahmed <shubhoahmed03@gmail.com>
2022-10-24 22:19:06 +0600

    Basic Routes Setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { RouterProvider } from 'react-router-dom'
+import { routes } from './Routes/Routes/Routes';
 
 function App() {
   return (
     <div className="App">
-
+      <RouterProvider routes={routes}></RouterProvider>
     </div>
   );
 }
